22547d5de984cd8b93749b3d12b1a3c10405ad08
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-04-22 15:47:54 +0700

    Xây dựng Logic phần Header #2 (use the API to eject the interface)

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,24 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import Image from '~/components/Images';
 
 const cx = classNames.bind(styles);
-const url_avatar = 'https://wallpapercave.com/wp/wp7477583.jpg';
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image className={cx('avatar')} src={url_avatar} alt="Hoaa" />
+        <Link to={`/:${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen Van A</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>nguyenvana</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
