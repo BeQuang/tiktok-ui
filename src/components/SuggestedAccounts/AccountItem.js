e75cff61d3d61eb2cf9d742deee93027dd277439
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-04-24 01:17:06 +0700

    Xây dựng UI phần sidebar #3 (Popper Suggested Accounts)

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -1,31 +1,46 @@
-import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountPreview from './AccountPreview';
 import styles from './SuggestedAccounts.module.scss';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return (
-        <div className={cx('account-item')}>
-            <img
-                className={cx('avatar')}
-                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/b2080dc0bfda1ddb8b23053480f3ecad.jpeg?lk3s=a5d48078&x-expires=1714050000&x-signature=QDS1DgmxQSiv%2Bg5Zf9XN5BEOEds%3D"
-                alt=""
-            ></img>
-            <div className={cx('item-info')}>
-                <p className={cx('nickname')}>
-                    <strong>quynhalee</strong>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                </p>
-                <p className={cx('name')}>Quỳnh Alee</p>
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
             </div>
+        );
+    };
+
+    return (
+        // Using a wrapper <div> or <span> tag around the reference element solves this by creating a new parentNode context.
+        <div>
+            <Tippy interactive delay={[800, 0]} offset={[-20, 0]} render={renderPreview} placement="bottom">
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/b2080dc0bfda1ddb8b23053480f3ecad.jpeg?lk3s=a5d48078&x-expires=1714050000&x-signature=QDS1DgmxQSiv%2Bg5Zf9XN5BEOEds%3D"
+                        alt=""
+                    ></img>
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>quynhalee</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Quỳnh Alee</p>
+                    </div>
+                </div>
+            </Tippy>
         </div>
     );
 }
-
-AccountItem.propTypes = {};
 
 export default AccountItem;
---
+++ src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,39 @@
+import classNames from 'classnames/bind';
+import styles from './AccountPreview.module.scss';
+import Button from '~/components/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountPreview() {
+    return (
+        <div className={cx('wrapper')}>
+            <header className={cx('header')}>
+                <img
+                    className={cx('avatar')}
+                    src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/b2080dc0bfda1ddb8b23053480f3ecad.jpeg?lk3s=a5d48078&x-expires=1714050000&x-signature=QDS1DgmxQSiv%2Bg5Zf9XN5BEOEds%3D"
+                    alt=""
+                />
+                <Button className={cx('follow-btn')} primary>
+                    Follow
+                </Button>
+            </header>
+            <div className={cx('body')}>
+                <p className={cx('nickname')}>
+                    <strong>quynhalee</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>Quỳnh Alee</p>
+                <p className={cx('analytics')}>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Followers</span>
+                    <strong className={cx('value')}>6.2M </strong>
+                    <span className={cx('label')}>Likes</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountPreview;
